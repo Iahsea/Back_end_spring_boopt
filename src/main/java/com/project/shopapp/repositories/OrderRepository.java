7267e0d046be6c7a9f51fed75e0238e3064d44b7
dovankhang <khangdv@blueoath.jp>
2025-07-03 13:31:53 +0700

    fix: get all order

--- src/main/java/com/project/shopapp/repositories/OrderRepository.java
+++ src/main/java/com/project/shopapp/repositories/OrderRepository.java
@@ -13,10 +13,15 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     // Tìm các đơn hàng của 1 user nào đó
     List<Order> findByUserId(Long userId);
 
-    @Query("SELECT o FROM Order o WHERE o.active = true AND (:keyword IS NULL OR :keyword = '' OR " +
-            "o.fullName LIKE %:keyword% " +
-            "OR o.address LIKE %:keyword% " +
-            "OR o.note LIKE %:keyword% " +
-            "OR o.email LIKE %:keyword%)")
+    @Query("""
+                SELECT o FROM Order o
+                WHERE o.active = true AND (
+                    :keyword IS NULL OR :keyword = '' OR
+                    LOWER(o.fullName) LIKE LOWER(CONCAT('%', :keyword, '%')) OR
+                    LOWER(o.address) LIKE LOWER(CONCAT('%', :keyword, '%')) OR
+                    LOWER(o.note) LIKE LOWER(CONCAT('%', :keyword, '%')) OR
+                    LOWER(o.email) LIKE LOWER(CONCAT('%', :keyword, '%'))
+                )
+            """)
     Page<Order> findByKeyword(@Param("keyword") String keyword, Pageable pageable);
 }
